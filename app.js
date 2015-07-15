3278b9b6abb1b72758ca08d664c99a5acc57e190
GTMichael <mshpirt@client-126.121.200.125.speedy.net.pe>
2015-07-15 17:03:59 -0400

    cookie tests

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var routes = require('./routes');
 var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var parseurl = require('parseurl')
+var MongoStore = require('connect-mongodb');
+var mongo = require('mongoose');
 
 var app = module.exports = express.createServer();
 
@@ -18,18 +22,94 @@ var ObjectId = require('mongodb').ObjectID;
 
 // Configuration
 
+/*
 var url = 'mongodb://localhost:27017/numbers';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
   db.close();
 });
+*/
+
+/* express/session tests */
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'secret1',
+  resave: false,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}));
+
+/*
+app.use(session({
+  genid: function(req) 
+    {
+    return genuuid() // use UUIDs for session IDs
+    },
+  resave: false,
+  saveUninitialized: true,
+  secret: 'secret1'
+}))
+*/
+
+/* express/session tests */
+app.use(function (req, res, next) {
+  var views = req.session.views
+
+  if (!views) {
+    views = req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  views[pathname] = (views[pathname] || 0) + 1
+
+  next()
+})
+
+/*
+var singleServer = {
+  "db" : "sessions",
+  "collection" : "express_sessions",
+  "host" : "localhost",
+  "port" : 27017
+}
+*/
+
+var conf = {
+  db: {
+    db: 'myDb',
+    host: '127.0.0.1',
+    port: 27017,  // optional, default: 27017
+    // username: 'admin', // optional
+    // password: 'secret', // optional
+    collection: 'sessions' // optional, default: sessions
+  },
+  secret: '076ee61d63aa10a125ea872411e433b9'
+};
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: conf.secret,
+    maxAge: new Date(Date.now() + 3600000),
+    store: new MongoStore(conf.db)
+  }));
+
+
+  /*
+  app.use(express.session({
+        secret: 'secret',
+        store: new mongoStore({'db': 'sessions'})
+    }));
+  */
+
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -63,16 +143,39 @@ app.get('/game', function (req, res) {
 */
 
 app.get('/game', function(req, res){
+  //console.log("Cookies: ", req.cookies);
+  //console.log("Cookie Value: ", req.cookies);
   if (req.cookies.remember) {
     res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
   } else {
     res.send('<form method="post"><p>Check to <label>'
       + '<input type="checkbox" name="remember"/> remember me</label> '
       + '<input type="submit" value="Submit"/>.</p></form>');
+    var doc = {name:"David", title: req.cookies};
+    console.log(doc);
+    /*
+    conf.users.insert(document, {w: 1}, function(err, records)
+      {
+      console.log("Record added as "+records[0]._id);
+      });
+    */
+
   }
 });
 
+app.get('/game2', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/game2'] + ' times ' + req.cookies.get)
+})
+
 app.post('/game', function(req, res){
+  console.log("Cookies: ", req.cookies);
+  var minute = 60 * 1000;
+  if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
+  res.redirect('back');
+});
+
+app.post('/game2', function(req, res){
+  console.log("Cookies: ", req.cookies);
   var minute = 60 * 1000;
   if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
   res.redirect('back');
@@ -87,6 +190,17 @@ app.get('/testpage', function (req, res) {
   res.render('testpage', { title: 'Test Page' });
 });
 
+var dbUrl = 'mongodb://';
+//dbUrl += conf.db.username + ':' + conf.db.password + '@';
+dbUrl += conf.db.host + ':' + conf.db.port;
+dbUrl += '/' + conf.db.db;
+mongo.connect(dbUrl);
+mongo.connection.on('open', function () {
+  app.listen(3000);
+});
+
+/*
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+*/
